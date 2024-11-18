6e13b78c70ccccc0659cac6d40956dccc69b52b6
Muhammad Zaid <mzaid.bese22seecs@student.nust.edu.pk>
2024-11-18 15:13:03 +0500

    lab9

--- src/poet/GraphPoet.java
+++ src/poet/GraphPoet.java
@@ -5,84 +5,180 @@ package poet;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.Set;
 
 import graph.Graph;
 
 /**
  * A graph-based poetry generator.
  * 
- * <p>GraphPoet is initialized with a corpus of text, which it uses to derive a
- * word affinity graph.
- * Vertices in the graph are words. Words are defined as non-empty
- * case-insensitive strings of non-space non-newline characters. They are
- * delimited in the corpus by spaces, newlines, or the ends of the file.
+ * <p>
+ * GraphPoet is initialized with a corpus of text, which it uses to derive a
+ * word affinity graph. Vertices in the graph are words. Words are defined as
+ * non-empty case-insensitive strings of non-space non-newline characters. They
+ * are delimited in the corpus by spaces, newlines, or the ends of the file.
  * Edges in the graph count adjacencies: the number of times "w1" is followed by
  * "w2" in the corpus is the weight of the edge from w1 to w2.
  * 
- * <p>For example, given this corpus:
- * <pre>    Hello, HELLO, hello, goodbye!    </pre>
- * <p>the graph would contain two edges:
- * <ul><li> ("hello,") -> ("hello,")   with weight 2
- *     <li> ("hello,") -> ("goodbye!") with weight 1 </ul>
- * <p>where the vertices represent case-insensitive {@code "hello,"} and
+ * <p>
+ * For example, given this corpus:
+ * 
+ * <pre>
+ *     Hello, HELLO, hello, goodbye!
+ * </pre>
+ * <p>
+ * the graph would contain two edges:
+ * <ul>
+ * <li>("hello,") -> ("hello,") with weight 2
+ * <li>("hello,") -> ("goodbye!") with weight 1
+ * </ul>
+ * <p>
+ * where the vertices represent case-insensitive {@code "hello,"} and
  * {@code "goodbye!"}.
  * 
- * <p>Given an input string, GraphPoet generates a poem by attempting to
- * insert a bridge word between every adjacent pair of words in the input.
- * The bridge word between input words "w1" and "w2" will be some "b" such that
- * w1 -> b -> w2 is a two-edge-long path with maximum-weight weight among all
- * the two-edge-long paths from w1 to w2 in the affinity graph.
- * If there are no such paths, no bridge word is inserted.
- * In the output poem, input words retain their original case, while bridge
- * words are lower case. The whitespace between every word in the poem is a
- * single space.
+ * <p>
+ * Given an input string, GraphPoet generates a poem by attempting to insert a
+ * bridge word between every adjacent pair of words in the input. The bridge
+ * word between input words "w1" and "w2" will be some "b" such that w1 -> b ->
+ * w2 is a two-edge-long path with maximum-weight weight among all the
+ * two-edge-long paths from w1 to w2 in the affinity graph. If there are no such
+ * paths, no bridge word is inserted. In the output poem, input words retain
+ * their original case, while bridge words are lower case. The whitespace
+ * between every word in the poem is a single space.
  * 
- * <p>For example, given this corpus:
- * <pre>    This is a test of the Mugar Omni Theater sound system.    </pre>
- * <p>on this input:
- * <pre>    Test the system.    </pre>
- * <p>the output poem would be:
- * <pre>    Test of the system.    </pre>
+ * <p>
+ * For example, given this corpus:
  * 
- * <p>PS2 instructions: this is a required ADT class, and you MUST NOT weaken
- * the required specifications. However, you MAY strengthen the specifications
- * and you MAY add additional methods.
- * You MUST use Graph in your rep, but otherwise the implementation of this
- * class is up to you.
+ * <pre>
+ *     This is a test of the Mugar Omni Theater sound system.
+ * </pre>
+ * <p>
+ * on this input:
+ * 
+ * <pre>
+ *     Test the system.
+ * </pre>
+ * <p>
+ * the output poem would be:
+ * 
+ * <pre>
+ *     Test of the system.
+ * </pre>
+ * 
+ * <p>
+ * PS2 instructions: this is a required ADT class, and you MUST NOT weaken the
+ * required specifications. However, you MAY strengthen the specifications and
+ * you MAY add additional methods. You MUST use Graph in your rep, but otherwise
+ * the implementation of this class is up to you.
  */
 public class GraphPoet {
-    
-    private final Graph<String> graph = Graph.empty();
-    
-    // Abstraction function:
-    //   TODO
-    // Representation invariant:
-    //   TODO
-    // Safety from rep exposure:
-    //   TODO
-    
-    /**
-     * Create a new poet with the graph from corpus (as described above).
-     * 
-     * @param corpus text file from which to derive the poet's affinity graph
-     * @throws IOException if the corpus file cannot be found or read
-     */
-    public GraphPoet(File corpus) throws IOException {
-        throw new RuntimeException("not implemented");
-    }
-    
-    // TODO checkRep
-    
-    /**
-     * Generate a poem.
-     * 
-     * @param input string from which to create the poem
-     * @return poem (as described above)
-     */
-    public String poem(String input) {
-        throw new RuntimeException("not implemented");
-    }
-    
-    // TODO toString()
-    
+
+	private final Graph<String> graph = Graph.empty();
+
+	// Abstraction function:
+	// graph represents the generated word affinity graph
+	// Representation invariant:
+	// graph isn't modified by any functions
+	// Safety from rep exposure:
+	// Graph<String> is never returned or modified, and is declared as private final
+
+	/**
+	 * Create a new poet with the graph from corpus (as described above).
+	 * 
+	 * @param corpus text file from which to derive the poet's affinity graph
+	 * @throws IOException if the corpus file cannot be found or read
+	 */
+	public GraphPoet(File corpus) throws IOException {
+		Scanner scanner = new Scanner(corpus);
+		if (scanner.hasNext()) {
+			String current = scanner.next().toLowerCase();
+			graph.add(current);
+			while (scanner.hasNext()) {
+				String next = scanner.next().toLowerCase();
+				int original = graph.set(current, next, 1);
+				if (original != 0) {
+					graph.set(current, next, original + 1);
+				}
+				current = next;
+			}
+		}
+		scanner.close();
+	}
+
+	/**
+	 * Get the generated word affinity graph
+	 * 
+	 * @return a copy of the generated word affinity graph
+	 */
+	public Graph<String> getGraph() {
+		Graph<String> result = Graph.empty();
+		for (String vertex : graph.vertices()) {
+			result.add(vertex);
+			Map<String, Integer> targets = graph.targets(vertex);
+			for (String target : targets.keySet()) {
+				result.set(vertex, target, targets.get(target));
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * Try to find a bridge word in the word affinity graph
+	 * 
+	 * @param current current word in the input poem
+	 * @param next    next word in the input poem
+	 * @return the bridge word if found, empty string if not found
+	 * @throws NoSuchElementException if not found
+	 */
+	private String findBridgeWord(String current, String next) throws NoSuchElementException {
+		current = current.toLowerCase();
+		next = next.toLowerCase();
+		Set<String> targets = graph.targets(current).keySet();
+		for (String middle : targets) {
+			if (graph.targets(middle).keySet().contains(next)) {
+				return middle;
+			}
+		}
+		throw new NoSuchElementException();
+	}
+
+	/**
+	 * Generate a poem.
+	 * 
+	 * @param input string from which to create the poem
+	 * @return poem (as described above)
+	 */
+	public String poem(String input) {
+		Scanner scanner = new Scanner(input);
+		List<String> words = new ArrayList<>();
+		if (scanner.hasNext()) {
+			String current = scanner.next();
+			words.add(current);
+			while (scanner.hasNext()) {
+				String next = scanner.next();
+				try {
+					words.add(findBridgeWord(current, next));
+				} catch (NoSuchElementException e) {
+				}
+				words.add(next);
+				current = next;
+			}
+		}
+		scanner.close();
+		return String.join(" ", words);
+	}
+
+	/**
+	 * @return string representation of the word affinity graph
+	 */
+	@Override
+	public String toString() {
+		return this.getGraph().toString();
+	}
+
 }
--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -5,6 +5,14 @@ package poet;
 
 import static org.junit.Assert.*;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.io.File;
+import java.io.IOException;
+
+import graph.Graph;
+
 import org.junit.Test;
 
 /**
@@ -13,13 +21,136 @@ import org.junit.Test;
 public class GraphPoetTest {
     
     // Testing strategy
-    //   TODO
+    //   constructor
+    //     corpus: exists, doesn't exist
+    //     words in contents: 0, 1, >1
+    //     words contain duplicates or don't
+    //   poem
+    //     words in input: 0, 1, >1
+    //     input word pairs in graph or not
     
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
     
-    // TODO tests
+    @Test
+    public void testGraphPoetNotFound() {
+        try {
+            File corpus = new File("test/poet/xyz.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            assert false; // should be unreachable
+        }
+        catch (IOException e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testGraphPoetEmpty() {
+        try {
+            File corpus = new File("test/poet/empty.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            Graph<String> graph = poet.getGraph();
+            assertEquals("empty graph expected",
+                    Collections.emptySet(), graph.vertices());
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+    @Test
+    public void testGraphPoetSingle() {
+        try {
+            File corpus = new File("test/poet/single.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            Graph<String> graph = poet.getGraph();
+            assertEquals("single vertex expected", Set.of("hello"), graph.vertices());
+            assertEquals("no edges from vertex expected", Collections.emptyMap(), graph.targets("hello"));
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+    @Test
+    public void testGraphPoetMultiple() {
+        try {
+            File corpus = new File("test/poet/multiple.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            Graph<String> graph = poet.getGraph();
+            assertEquals("2 vertices expected", Set.of("hello,", "goodbye!"), graph.vertices());
+            assertEquals("expected edges from 'hello,' ", Map.of("hello,", 2, "goodbye!", 1), graph.targets("hello,"));
+            assertEquals("no edges from 'goodbye!' expected", Collections.emptyMap(), graph.targets("goodbye!"));
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+    @Test 
+    public void testGraphPoetPoemEmpty() {
+        try {
+            File corpus = new File("test/poet/phrases.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            String input = "";
+            assertEquals("empty poem expect", input, poet.poem(input));
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+    @Test
+    public void testGraphPoetPoemSingle() {
+        try {
+            File corpus = new File("test/poet/phrases.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            String input = "hello";
+            assertEquals("expect identical poem", input, poet.poem(input));
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+    @Test
+    public void testGraphPoetPoemWithBridgeWords() {
+        try {
+            File corpus = new File("test/poet/phrases.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            
+            // bridge word is quickly
+            String input1 = "The quick brown fox ran away from the ocean.";
+            assertEquals("expected poem", "The quick brown fox ran quickly away from the ocean.", poet.poem(input1));
+            
+            // 3 bridge words: fast, jumped, lazy
+            String input2 = "I saw a red fox over the dog.";
+            assertEquals("expected poem", "I saw a fast red fox jumped over the lazy dog.", poet.poem(input2));
+            
+            // no bridge word
+            String input3 = "The quick dog ran.";
+            assertEquals("expected poem", "The quick dog ran.", poet.poem(input3));
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
+
+
+    @Test
+    public void testGraphPoetToString() {
+        try {
+            File corpus = new File("test/poet/multiple.txt");
+            GraphPoet poet = new GraphPoet(corpus);
+            assertEquals("expected graph with multiple edges to string", "(hello, -> hello,, 2)\n(hello, -> goodbye!, 1)", poet.toString());
+        }
+        catch (IOException e) {
+            assert false; // should be unreachable
+        }
+    }
     
+    
+
 }
